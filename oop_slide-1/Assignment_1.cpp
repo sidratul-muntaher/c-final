cce94f489ac6c20c0e809a6b0944a9714919d4ee
SIDTATUL <19103073@iubat.edu>
2019-12-11 13:28:33 +0600

    Silde one added

---
+++ oop_slide-1/Assignment_1.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "6.62" << 9.34<< 99<< 110 << endl;
+}
---
+++ oop_slide-1/oop1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "Object-Oriented Programming" <<endl;
+    cout << 100.99 <<endl;
+    int num;
+    cin >> num;
+    cout << num;
+    return 0;
+}
